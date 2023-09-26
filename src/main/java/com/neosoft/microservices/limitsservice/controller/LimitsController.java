b501f399d3ef17ecb2974fd42214f19e8448fef2
amarbhore4141 <meetamarbhore@gmail.com>
2023-09-26 16:08:52 +0530

    reading values from Application.properties file, mapped to config class

--- src/main/java/com/neosoft/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/neosoft/microservices/limitsservice/controller/LimitsController.java
@@ -1,6 +1,8 @@
 package com.neosoft.microservices.limitsservice.controller;
 
 import com.neosoft.microservices.limitsservice.beans.Limitsconfiguration;
+import com.neosoft.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
 
     @GetMapping("/limitsHardcoded")
-    public Limitsconfiguration retrieveLimitsFromConfiguration(){
-        return new Limitsconfiguration(1,1000);
+    public Limitsconfiguration retrieveLimitsFromConfiguration() {
+        return new Limitsconfiguration(1, 1000);
     }
+
+    @GetMapping("/limitsDynamic")
+    public Limitsconfiguration limitsFromApplicationPropertisFileAndConfigClass() {
+        return new Limitsconfiguration(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+
 }
